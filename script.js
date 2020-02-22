bad1f833aca39dae13eb4e514765444d68e79414
Mukola <mukola.kutsil22@gmail.com>
2020-02-22 22:22:36 +0200

    animation_1

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-alert("dwaddwwa");
+let img = document.querySelector(".cx-5"),
+    div = document.querySelector(".img"),
+    btn = document.querySelector(".btn");
+
+
+
+ let myAnimation = function() {
+     let pos = 0;
+
+     console.log("dwad");
+
+     let id = setInterval(frame, 100);
+
+     function frame() {
+        if (pos == 3000) {
+          clearInterval(id);
+       div.style.backgroundColor =   'blue';
+    //    div.style.left =  "3000px";
+       
+        } else{
+        
+            pos += 300;
+       div.style.left =  pos + 'px';
+       div.style.top =  pos + 'px';
+       div.style.backgroundColor =   'red';
+    //    div.style.backgroundColor =   'blue';
+
+
+
+       console.log(pos);
+
+      }
+    }
+   };
+btn.addEventListener("click", myAnimation);
